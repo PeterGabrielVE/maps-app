d45de6709e64ef6922cf869813f74598ff3c867d
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-31 21:10:24 -0400

    token

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 
 import mapboxgl from 'mapbox-gl'; 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja3hramV2OWIwbjEwMzFwYzJlZWl6N2g5In0.iKXPpYvo7UPRiiZ-x_lCrw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2Fib2xlYWwxMjMiLCJhIjoiY2x1ZWptY3V2MXE5MDJsbWZncGpvYmNlOSJ9.OyOg62tn2IjgqFtjPLGkhQ';
 
 if ( !navigator.geolocation ) {
     alert('Tu navegador no soporta el GeoLocation');
